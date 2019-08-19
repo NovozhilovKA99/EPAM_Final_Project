59cc5c8acb226d077f5b7ad4b720d413b65a3843
Novozhilov Konstantin <Kostyansa@gmail.com>
2019-08-19 18:19:51 +0400

    Deleted springdata

--- src/main/java/com/citytransportsystem/repository/springdata/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.citytransportsystem.repository.springdata;
-
-import com.citytransportsystem.dto.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
